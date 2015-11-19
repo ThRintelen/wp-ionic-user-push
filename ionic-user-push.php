b4711e528edacc816552d37c05268e1dde109eb9
Thorsten Rintelen <t.rintelen@traperto.com>
2015-11-19 18:43:26 +0100

    Inital commit

---
+++ ionic-user-push.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Ionic User Push Notification
+Plugin URI: tbd
+Description: Send push notifications to ionic users
+Version: 1.0
+Author: Thorsten Rintelen
+Author URI: http://www.clever-code.de
+License: GPLv2
+*/
